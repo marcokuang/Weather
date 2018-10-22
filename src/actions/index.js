87e06ff9aa7e16ee482488fa7dcce8277a4f75ec
Marco Kuang <marcokuang@Marcos-MacBook-Pro.local>
2018-10-21 18:29:14 -0700

    added async request functionality via axios

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+const API_KEY = '6c1870426b84a00e59b637cafe06fcc5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+import axios from 'axios';
+
+export const FETCH_WEATHER = 'Fetch_Weather';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
