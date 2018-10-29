7699241f4efea294f7ca6d44cc4db02b59e48e5b
Marco K <marcokuang@users.noreply.github.com>
2018-10-28 21:11:58 -0700

    Fix an error where an invalid API response will cause the app to crash

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => {
+      return (weather.main.temp - 273.75) * 9 / 5 + 32;
+    });
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map (weahter => weahter.main.humidity);
+
     return (
       <tr key={name}>
         <td>
           {name}
+        </td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
+        <td>
+          <Chart data={pressures} color="green" />
+        </td>
+        <td>
+          <Chart data={humidities} color="purple" />
         </td>
       </tr>
     )
@@ -19,9 +35,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
+            <th>Temperature (F)</th>
             <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -6,7 +6,13 @@ export default function(state = [], action){
   switch (action.type) {
     case FETCH_WEATHER:
       // return state.concat([action.payload.data]);
-      return [action.payload.data, ...state]; //return [city, city, city] NOT [city, [city] ]
+      if(action.payload.data){
+        return [action.payload.data, ...state]; //return [city, city, city] NOT [city, [city] ]
+      }
+      else{
+        return state;
+      }
+      
     default:
       return state;
   }
